cbcce795fa209e5443e27295923cbe5737ddbed2
akudryavtseva <elenka_k@mail.ru>
2018-07-24 13:14:29 +0300

    add PHP1/zadanie1

---
+++ PHP1/zadanie1/index5.php
@@ -0,0 +1,11 @@
+<?php
+$a = 1;
+$b = 2;
+echo '$a = ' . $a . "<br/>" ;
+echo '$b = ' . $b . "<br/>". "<br/>";
+$a = $a + $b;
+$b = $a - $b;
+$a = $a - $b;
+echo '$a = ' . $a . "<br/>" ;
+echo '$b = ' . $b;
+
